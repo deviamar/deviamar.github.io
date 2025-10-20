430fc394780b1a1bb7391c8f5f046beca6f569b0
Devi <90939601+deviamar@users.noreply.github.com>
2025-10-19 17:14:49 -0700

    Update and rename App.js to App.jsx
    Changing into more modular design

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Research from './pages/Research';
+import Outreach from './pages/Outreach';
+import Awards from './pages/Awards';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-800">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/outreach" element={<Outreach />} />
+        <Route path="/awards" element={<Awards />} />
+      </Routes>
+    </div>
+  );
+}
