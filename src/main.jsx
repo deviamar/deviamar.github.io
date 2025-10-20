b7be55493201d1064b3e4e73fbea274ae3691269
Devi <90939601+deviamar@users.noreply.github.com>
2025-10-19 20:48:37 -0700

    Updated basename in BrowserRouter to "/"

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/web-ui">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
